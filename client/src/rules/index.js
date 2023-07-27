52589f6479444a4ff22f4c1acef8da591c7fd632
Ryan <113010586+b11901040@users.noreply.github.com>
2023-07-27 21:13:06 +0800

    .

--- client/src/rules/index.js
+++ client/src/rules/index.js
@@ -1,19 +1,20 @@
 const rules = (from, to, piece, capturedPiece) => {
     console.log(`from`, from, `to`, to, `piece`, piece, `capturedPiece`, capturedPiece);
-    const color = piece[0];
+    const isWhite = (piece[0] === 'w');
     const type = piece[1];
 
     // can't capture own pieces
-    if (color === capturedPiece[0]) return false;
+    if (piece[0] === capturedPiece[0]) return false;
 
     switch (type) {
         case 'p':
             // move two squares forward at start
-            if (capturedPiece === '' && from[0] === 1 + 5 * (color === 'w') && to[0] === 3 + (color === 'w')) return true
+            if (capturedPiece === '' && from[0] === 1 + 5 * isWhite && to[0] === 3 + isWhite) return true
             // move one square forward
-            if (capturedPiece === '' && from[0] === to[0] - 1 + 2 * (color === 'w') && from[1] === to[1]) return true
+            if (capturedPiece === '' && from[0] === to[0] - 1 + 2 * isWhite && from[1] === to[1]) return true
             // capture pieces
-            return from[0] === to[0] - 1 + 2 * (color === 'w') && Math.abs(from[1] - to[1]) === 1;
+            return from[0] === to[0] - 1 + 2 * isWhite && Math.abs(from[1] - to[1]) === 1;
+        // todo: en passant
         case 'n':
             console.log(`n`);
             return true
