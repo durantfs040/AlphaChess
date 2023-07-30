f272afa399be8e1ed43ca17c624eccb048f7f078
DhgfLu <xm3ru8p@gmail.com~>
2023-07-30 17:25:02 +0800

    :]

--- client/src/components/Row.jsx
+++ client/src/components/Row.jsx
@@ -1,11 +1,12 @@
 import Piece from "./Piece.jsx";
+import {useChess} from "../hooks/useChess.jsx"
 
 const Row = ({rowPositions, rank}) => {
-
+    const {positionFrom} = useChess()
     return (
         <div className="row">
             {rowPositions.map((piece, file) => <div className="square"
-                                                    style={{background: (rank + file) % 2 === 0 ? '#fde6bf' : '#c78443'}}
+                                                    style={{background: (positionFrom[0] === rank && positionFrom[1] === file)? 'green' : (rank + file) % 2 === 0 ? '#fde6bf' : '#c78443'}}
                                                     key={file}>
                 <Piece piece={piece} position={[rank, file]}/>
             </div>)}
