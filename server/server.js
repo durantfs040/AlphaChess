ba96a8f0d217f04b61262f8fae2b391b92cb9e7d
Ryan <113010586+b11901040@users.noreply.github.com>
2023-08-12 15:56:52 +0800

    .

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.json('ok');
+})
+
 app.post('/analyze', (req, res) => {
     const position = req.body.position;
     try {
